4cd03e93aba4ffe3824a3197a058d38aa51d0387
bogdandm <bogdan.dm1995@yandex.ru>
2015-09-04 21:15:34 +0300

    add xml configuration file

--- ai.py
+++ ai.py
@@ -1,3 +1,4 @@
+import xml.dom.minidom as dom
 from math import factorial
 
 from engine import *
@@ -7,7 +8,44 @@ from engine import *
 
 
 class AI:
-	def __init__(self, game: Game, hand_number: int):
+	def __init__(self, game: Game, hand_number: int, settings_path='./settings/template.xml'):
+		xml = dom.parse(settings_path)
+		self.settings = {
+			'all': {
+				'trump_suit_penalty':
+					int(xml.getElementsByTagName('all')[0].
+						getElementsByTagName('trump_suit_penalty')[0].childNodes[0].data)
+			},
+			'attack': {
+				'pair_bonus':
+					int(xml.getElementsByTagName('attack')[0].
+						getElementsByTagName('pair_bonus')[0].childNodes[0].data),
+				'coefficient_of_probability':
+					int(xml.getElementsByTagName('attack')[0].
+						getElementsByTagName('coefficient_of_probability')[0].childNodes[0].data),
+				'limit':
+					int(xml.getElementsByTagName('attack')[0].
+						getElementsByTagName('limit')[0].childNodes[0].data),
+			},
+			'defense': {
+				'coefficient_of_probability':
+					int(xml.getElementsByTagName('defense')[0].
+						getElementsByTagName('coefficient_of_probability')[0].childNodes[0].data),
+				'coefficient_of_stage':
+					int(xml.getElementsByTagName('defense')[0].
+						getElementsByTagName('coefficient_of_stage')[0].childNodes[0].data),
+				'limit':
+					int(xml.getElementsByTagName('defense')[0].
+						getElementsByTagName('limit')[0].childNodes[0].data),
+				'count_of_cards':
+					int(xml.getElementsByTagName('defense')[0].
+						getElementsByTagName('count_of_cards')[0].childNodes[0].data),
+				'limit2':
+					int(xml.getElementsByTagName('defense')[0].
+						getElementsByTagName('limit2')[0].childNodes[0].data),
+			}
+		}
+
 		self.game = game
 		self.hand = game.hand[hand_number]
 		self.hand_number = hand_number
@@ -43,7 +81,7 @@ class AI:
 
 		if mode == 'TABLE' or mode == 'ALL':
 			if inf != self.hand_number:
-				buf=[]
+				buf = []
 				for i in range(len(self.enemy_cards)):
 					if card == self.enemy_cards[i]:
 						buf.append(i)
@@ -67,7 +105,7 @@ class AI:
 						if card2 is not None and card.number == card2.number:  # Если можно подкидывать
 							can_attack = True
 							if card.suit == self.game.trump_suit:
-								sums[-1][1] += -5
+								sums[-1][1] += self.settings['all']['trump_suit_penalty']
 						"""if stage < 50 and card2.weight(self.game.trump_suit) > 20:
 							# В начале игры сливаем крупные карты противника
 							return -1"""
@@ -82,11 +120,12 @@ class AI:
 
 			# if card.number < 11 or stage > 75: # до поздней игры бережем крупные карты
 			for card2 in self.hand:  # Ищем пары (тройки)
-				if card != card2 and card.number == card2.number and card2.suit != self.game.trump_suit:
-					sums[-1][1] += 10
-					print('pair')
+				if card != card2 and card.number == card2.number:
+					if card.suit != self.game.trump_suit and card2.suit != self.game.trump_suit:
+						sums[-1][1] += self.settings['attack']['pair_bonus']
+						print('pair')
 
-			k = 2
+			k = self.settings['attack']['coefficient_of_probability']
 			sums[-1][1] *= self.probability(card) ** (2 * stage / 100) / k + (1 - 1 / k)
 			# !!! Попробовать 1 - вер-сть при битве ИИ (для надежности)
 
@@ -96,7 +135,7 @@ class AI:
 		for tmp in sums:
 			print('%s|%f' % (tmp[0], tmp[1]))
 		r = self.hand.index(result[0])
-		return r if result[1] > 5 or not self.game.table else -1
+		return r if not self.game.table or result[1] > self.settings['attack']['limit'] else -1
 
 	def defense(self, card: Card):
 		stage = (1 - self.game.set.remain() / 39) * 100
@@ -106,26 +145,13 @@ class AI:
 			sums.append([card_, 0])
 
 			if card_.more(card, self.game.trump_suit):
-				sums[-1][1] += 27 - card_.weight(self.game.trump_suit) - (
-					5 if card_.suit == self.game.trump_suit else 0)
+				sums[-1][1] += 27 - card_.weight(self.game.trump_suit) - (self.settings['all']['trump_suit_penalty']
+																		  if card_.suit == self.game.trump_suit else 0)
 				# if card.number == card_.number: sums[-1][1] += 10
 
-				"""tmp = difference([Card(card_.number, x) for x in range(1, 5)], [card_])  # Три карты того же достоинства
-				if not (set(tmp) & set(self.unknown_cards)):  # Карты того же достоинства найдены
-					tmp2 = set(tmp) & set(self.enemy_cards)  # Карты того же достоинства у противника
-					if not tmp2 and card_.weight() < 19:
-						# Если это карта меньше козырной 6ки и противник не будет подкидывать
-						sums[-1][1] += 20
-					elif tmp2:  # Если карты противник может подкидывать
-						if Card(card_.number, self.game.trump_suit) not in list(tmp2):
-							# Если у противника НЕ козырь того же достоинства
-							sums[-1][1] += -20
-						elif set(difference(tmp, [card_, Card(card_.number, self.game.trump_suit)])) & \
-								set(self.enemy_cards):
-							# Если у него только козырь того же достоинства
-							pass"""
-				k = 2
-				sums[-1][1] *= self.probability_throw_up(card_) ** (stage / 100 / 2 + 1) / k + (1 - 1 / k)
+				k = self.settings['defense']['coefficient_of_probability']
+				sums[-1][1] *= self.probability_throw_up(card_) ** \
+							   (stage / 100 / 2 + self.settings['defense']['coefficient_of_stage']) / k + (1 - 1 / k)
 			# !!! Попробовать 1 - вер-сть при битве ИИ (для надежности)
 			else:
 				sums[-1][1] = -1000
@@ -136,7 +162,11 @@ class AI:
 		for tmp in sums:
 			print('%s|%f' % (tmp[0], tmp[1]))
 		r = self.hand.index(result[0])
-		return r if (result[1] > 0 or (len(self.table_cards) >= 4 and result[1] > -10)) else -1
+		return r if (
+			result[1] > self.settings['defense']['limit'] or (
+				len(self.table_cards) >= self.settings['defense']['count_of_cards'] and
+				result[1] > self.settings['defense']['limit2'])
+		) else -1
 
 	def probability(self, card):
 		# Вероятность того, что противник НЕ сможет побить card
