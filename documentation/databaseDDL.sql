2be886d59a98ea0a7eb5225e8f9aed7bb9074327
bogdandm <bogdan.dm1995@yandex.ru>
2015-11-17 17:59:25 +0300

    add registration page (html & css only) change DDL

--- documentation/databaseDDL.sql
+++ documentation/databaseDDL.sql
@@ -7,7 +7,11 @@ CREATE TABLE users
 (
     id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL,
-    pass_sha256 VARCHAR(300) NOT NULL
+    pass_sha256 VARCHAR(300) NOT NULL,
+    is_admin TINYINT NOT NULL,
+    is_activated TINYINT DEFAULT 1 NOT NULL,
+    has_avatar TINYINT DEFAULT 0 NOT NULL,
+    email VARCHAR(1024)
 );
 ALTER TABLE sessions ADD FOREIGN KEY (user_id) REFERENCES users (id);
 CREATE UNIQUE INDEX unique_user_id ON sessions (user_id);
