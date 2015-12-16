ef1e4deba9703242c5ab1c6d19340553e6f1f997
bogdandm <bogdan.dm1995@yandex.ru>
2015-12-16 15:32:40 +0300

    Final commit for v1.0

--- run_server.py
+++ run_server.py
@@ -11,17 +11,14 @@ def get_ip():
 						   shell=True, stdout=subprocess.PIPE).stdout.read()
 	if not res:
 		res = subprocess.Popen("curl.exe ifconfig.me --connect-timeout 5",
-						   shell=True, stdout=subprocess.PIPE).stdout.read()
+							   shell=True, stdout=subprocess.PIPE).stdout.read()
 	return res.decode()[:-1]
 
 
 if __name__ == "__main__":
 	outer_ip = get_ip()
 	ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
-	flask_server = Server(
-		ip, outer_ip,
-		seed=None
-	)
+	flask_server = Server(ip, outer_ip, seed=None)
 	print(ip)
 	print(outer_ip)
 	server = WSGIServer((ip, 80), flask_server.app)
