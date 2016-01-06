b6dd2dbebbed1f4b65b80a51a77670f343713deb
bogdandm <bogdan.dm1995@yandex.ru>
2016-01-06 21:36:43 +0300

    clean

--- tests/massive_1.py
+++ tests/massive_1.py
@@ -3,14 +3,17 @@ import time
 from engine.ai import AI
 from engine.engine import Game
 
-result = [0, 0, 0]
-for i in range(10000):
+COUNT = 100000
+
+result = [0, 0, 0, 0]
+for i in range(COUNT):
 	seed = i * int(time.time())
 	# print(seed)
 
 	game = Game(save_changes=False, log_on=False, seed=seed)
-	ai0 = AI(game, 0, './engine/settings/end_game_on.xml')
+	ai0 = AI(game, 0)
 	ai1 = AI(game, 1)
+	t1 = time.time()
 	while game.can_play() is None:
 		if game.turn:
 			x = ai1.attack()
@@ -39,6 +42,8 @@ for i in range(10000):
 			if not game.can_continue_turn():
 				game.switch_turn([ai0, ai1])
 
+	t2 = time.time()
+	result[3] += (t2 - t1) * 1000
 	a = game.result
 	if a is not None:
 		if a != -1:
@@ -46,6 +51,6 @@ for i in range(10000):
 		else:
 			result[2] += 1
 
-	print('%.4f%%' % (result[0] / (result[0] + result[1])))
+	# print('%.4f%%' % (result[0] / (result[0] + result[1])))
 
-print('AI0: %i, AI1: %i, Draw: %i' % (result[0], result[1], result[2]))
+print('AI0: %i, AI1: %i, Draw: %i\nAvg. time: %i ms' % (result[0], result[1], result[2], result[3] / COUNT))
