926cee8b24bf71d33cb5ecd3f4226fdee58ae7d3
bogdandm <bogdan.dm1995@yandex.ru>
2016-02-23 15:50:23 +0300

    _

--- line_counter.py
+++ line_counter.py
@@ -1,22 +1,24 @@
-from json import dumps
 from os import listdir
 from os.path import isfile, join, splitext
 
 ignore = [
-	'.git', 'kcachegrind', 'settings', 'log', 'logs', '.idea',
-	'documentation', 'extra', '__pycache__', 'avatar', 'svg', 'unit_tests',
+	'.git', 'kcachegrind', 'settings', '.idea',
+	'documentation', 'extra', '__pycache__', 'unit_tests',
 
-	'highcharts.js', 'text.txt'
+	'highcharts.js'
 ]
+
+extensions = ['.py', '.html', '.js', '.css']
 
 
 def get_files_list(l: list, path):
 	for file in listdir(path):
-		if file in ignore or file.find('jquery') != -1 or file.find('favicon') != -1:
+		if file in ignore or file.find('jquery') != -1:
 			continue
 		path2 = join(path, file)
 		if isfile(path2):
-			l.append(path2)
+			if splitext(file)[1] in extensions:
+				l.append(path2)
 		else:
 			get_files_list(l, path2)
 
@@ -31,15 +33,10 @@ def file_len(fname):
 
 l = []
 get_files_list(l, ".")
-#open('text.txt', 'w').write(dumps(l, separators=(', \n', ': ')))
+# open('text.txt', 'w').write(dumps(l, separators=(', \n', ': ')))
 
-results = {
-	'.py': 0,
-	'.html': 0,
-	'.js': 0,
-	'.css': 0,
-	'all': 0
-}
+results = {ext: 0 for ext in extensions}
+results['all']=0
 
 for file in l:
 	print(file)
