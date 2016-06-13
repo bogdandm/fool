a3cbac6d9b03b4bb0ac91590ffb31dfd168c869b
bogdandm <bogdan.dm1995@yandex.ru>
2016-06-13 15:37:25 +0300

    command line args

--- run_server.py
+++ run_server.py
@@ -1,11 +1,16 @@
 import socket
+import sys
 
 from gevent.wsgi import WSGIServer
 
 from server.server import Server
 
 
-def get_ip():
+def get_argk(argv: list) -> dict:
+	return dict(zip(argv[1::2], argv[2::2]))
+
+
+def get_outer_ip():
 	import subprocess
 	res = subprocess.Popen("curl.exe ifconfig.co --connect-timeout 5",
 						   shell=True, stdout=subprocess.PIPE).stdout.read()
@@ -16,10 +21,21 @@ def get_ip():
 
 
 if __name__ == "__main__":
-	outer_ip = get_ip()
-	ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
+	argk = get_argk(sys.argv)
+	if "-l" in argk:
+		ip = "0.0.0.0"
+		outer_ip = "localhost"
+	else:
+		if "-domen" in argk:
+			outer_ip = argk["-d"]
+		else:
+			outer_ip = get_outer_ip()
+		if "-ip" in argk:
+			ip = argk["-ip"]
+		else:
+			ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
 	flask_server = Server(ip, outer_ip, seed=None)
-	print(ip)
-	print(outer_ip)
+	print("http://" + ip)
+	print("http://" + outer_ip)
 	server = WSGIServer((ip, 80), flask_server.app)
 	server.serve_forever()
